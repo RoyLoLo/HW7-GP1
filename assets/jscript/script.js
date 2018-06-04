46200bc255804300d358ad7808b5a44408993592
Dopam1ne <roywhoa@gmailcom>
2018-06-04 08:21:56 -0700

    sample modals done

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,28 +1,45 @@
 
-
-
-
+//can also use $(documnent).ready(function(){});
+$(function(){
 //Modal stuff
-var modal = document.getElementById("start-modal");
+var modalStart = document.getElementById("start-modal");
 var modalBtn = document.getElementById("modal-btn");
 var closeBtn = document.getElementsByClassName("start-btn")[0];
-
-//will load startmodal on window load
+var tempImage = document.getElementById("temp-image");
+var imageModal = document.getElementById("image-modal");
+var imageClose = document.getElementsByClassName("cls-btn")[0];
+//will load start-modal on window load
 window.onload = openModal();
+imageModal.style.display = "none";
 
-modalBtn.addEventListener("click",openModal);
-closeBtn.addEventListener("click",closeModal);
-window.addEventListener("dblclick", oustideClick);
+$(modalBtn).on("click",openModal);
+$(closeBtn).on("click",closeModal);
+$(tempImage).on("click",openTempImageModal);
+$(imageClose).on("click",closeImageModal);
+
 
 function openModal(){
-    modal.style.display = "block";
+    modalStart.style.display = "block";
 }
 
 function closeModal(){
-    modal.style.display = "none";
+    modalStart.style.display = "none";
+}
+
+function openTempImageModal(){
+    imageModal.style.display = "block"
 }
 
 
+function closeImageModal(){
+    imageModal.style.display = "none";
+}
 
 //end of modal stuff
+
+});//end of docuemnt ready funtion
+
+
+
+
 
