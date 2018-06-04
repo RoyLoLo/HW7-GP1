1a899a85611f026ef61849824c7e6b72ee79cb54
Dopam1ne <roywhoa@gmailcom>
2018-06-04 14:46:36 -0700

    populate not yet completed

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,47 +1,68 @@
-<<<<<<< HEAD
+
+
 $.ajax({
     url:"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b44fe26f44f54697aa4d45c7d56ac36b",
     method:"GET"
 }).then(function(response){
-    console.log(response);
+    //console.log(response);
     var results=response.articles;
-    for(var i=0;i<10;i++){
+    for(var i=0;i<9;i++){
     var topic= results[i].title;
     var desc= results[i].description;
     var url= results[i].url;
     var source= results[i].source.name;
-    console.log(source);
-    console.log(topic);
-    console.log(desc);
-    console.log(url);
+    //console.log(source);
+    //console.log(topic);
+    //console.log(desc);
+    //console.log(url);
+    
    
     $.ajax({
         url:"https://cors-anywhere.herokuapp.com/https://www.google.com/search?q="+topic+"&safe=active&source=lnms&tbm=isch",
         method:"GET"
     }).then(function(innerResponse){
         var images = innerResponse.match(/https[^"]*jpg/);
-        console.log(images);
-        
         var imagesDiv=$("<div class='images'>")
         var topicImage=$("<img>");
 
         topicImage.addClass("image");
         topicImage.attr("src", images);
         topicImage.attr("data-count", 0);
+        topicImage.attr("data-topic", topic);
+        topicImage.attr("data-desc", desc);
+        topicImage.attr("data-url", url);
+        topicImage.attr("data-source", source);
         imagesDiv.append(topicImage);
         
 
 
         $(".imageDump").append(imagesDiv);
-    })
-    }
+    });
     
+    }//for loop
+    
+    $(document).on("click",".images",function(){
+        console.log(this);
+        var modalArticle = $("<p>");
+        var modallink = $("<a>");
 
+        modalArticle.html(this.data-desc);
+        $("#image-modal").append(modalArticle);
+
+                
+        
+        
+        
+        openImageModal();
+
+        
+        
+    })
 })
-=======
+
 
 //can also use $(documnent).ready(function(){});
-$(function(){
+//$(function(){
 //Modal stuff
 var modalStart = document.getElementById("start-modal");
 var modalBtn = document.getElementById("modal-btn");
@@ -55,7 +76,7 @@ imageModal.style.display = "none";
 
 $(modalBtn).on("click",openModal);
 $(closeBtn).on("click",closeModal);
-$(tempImage).on("click",openTempImageModal);
+
 $(imageClose).on("click",closeImageModal);
 
 
@@ -67,7 +88,7 @@ function closeModal(){
     modalStart.style.display = "none";
 }
 
-function openTempImageModal(){
+function openImageModal(){
     imageModal.style.display = "block"
 }
 
@@ -78,10 +99,9 @@ function closeImageModal(){
 
 //end of modal stuff
 
-});//end of docuemnt ready funtion
+//});//end of docuemnt ready funtion
 
 
 
 
 
->>>>>>> html_framework
