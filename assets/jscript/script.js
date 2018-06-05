930e4d0cfb161b18af37321659f2e18f8375c0de
Ji Wang <tomw0621@gmail.com>
2018-06-05 15:02:25 -0700

    displays data-count in modal now

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,4 +1,37 @@
-var config = {
+//adding background image to start page
+var splashImage =["assets/images/trump.jpg", "assets/images/army.jpg", "assets/images/olympics.jpg", "assets/images/africa.jpg", "assets/images/un.jpg"];
+
+var rand = splashImage[Math.floor(Math.random()*splashImage.length)];
+
+$(".bgImage").attr("src", rand);
+
+
+function imgError(image) {
+    image.onerror="";
+    image.src="assets/images/missing.jpg"
+    return true;
+}
+//running ajax call for top news of the day
+$.ajax({
+    url:"https://newsapi.org/v2/top-headlines?country=us&apiKey=b44fe26f44f54697aa4d45c7d56ac36b",
+    method:"GET"
+}).then(function(response){
+
+    //console.log(response);
+
+    var results=response.articles;
+    console.log(response)
+    var results=response.articles;
+//forloop to pull 10 headlines fom the day 
+    for(let i=0;i<9;i++){
+    let topic= results[i].title;
+    let desc= results[i].description;
+    let url= results[i].url;
+    let source= results[i].source.name;
+    //console.log(source);
+    //console.log(topic);
+    //console.log(desc);
+    //console.log(url);levar config = {
     apiKey: "AIzaSyBllNmEjRqK_vUQf632WNBD3TO-E-PtZRQ",
     authDomain: "dailyfails-afad0.firebaseapp.com",
     databaseURL: "https://dailyfails-afad0.firebaseio.com",
@@ -151,6 +184,118 @@ $.ajax({
   // console.log($(this).parent().find('span'));
   // $(this).parent().find('span').html(clickCount);
   $('#inspected').html(clickCount);
+                
+        
+        
+        
+        openImageModal();
+
+})
+
+$(document).on("click", ".images", function(){
+    // console.log(this);s
+
+});
+
+//can also use $(documnent).ready(function(){});
+//Modal stuff
+var modalStart = document.getElementById("start-modal");
+var modalBtn = document.getElementById("modal-btn");
+var closeBtn = document.getElementsByClassName("startImage")[0];
+var imageModal = document.getElementById("image-modal");
+var imageClose = document.getElementsByClassName("cls-btn")[0];
+//will load start-modal on window load
+window.onload = openModal();
+imageModal.style.display = "none";
+
+$(modalBtn).on("click",openModal);
+$(closeBtn).on("click",closeModal);
+$(imageClose).on("click",closeImageModal);
+
+
+function openModal(){
+    modalStart.style.display = "block";
+}
+
+function closeModal(){
+    modalStart.style.display = "none";
+}
+
+function openImageModal(){
+    imageModal.style.display = "block"
+}
+
+
+function closeImageModal(){
+    imageModal.style.display = "none";
+}
+
+//end of modal stuff
+
+});//end of docuemnt ready funtiont
+
+
+//ajax for pulling news images
+    $.ajax({
+        url:"https://cors-anywhere.herokuapp.com/https://www.google.com/search?q="+topic+"&safe=active&source=lnms&tbm=isch",
+        method:"GET"
+    }).then(function(innerResponse){
+        
+        let images = innerResponse.match(/https[^"]*\.jpg/);
+        let imagesDiv=$("<div class='images'>")
+        let topicImage=$("<img>");
+        var pCount=$("<p>");
+        var headLiner=desc.replace(/[^A-Z0-9]+/ig, "_")
+        topicImage.addClass("image");
+        topicImage.attr("onerror", "imgError(this)");
+        topicImage.attr("src", images[0]);
+        topicImage.attr("data-count", 0);
+        topicImage.attr("data-topic", topic);
+        topicImage.attr("data-desc", desc);
+        topicImage.attr("data-headLine", headLiner);
+        topicImage.attr("data-url", url);
+        topicImage.attr("data-source", source);
+        imagesDiv.append(topicImage);
+        var daCount=$("<span>");
+        pCount.text("Times inspected: ");
+        pCount.append(daCount);
+        imagesDiv.append(pCount);
+        
+        
+        $(".imageDump").append(imagesDiv);
+    });
+
+    }
+    
+    $(document).on("click",".image",function(){
+        console.log(this);
+        $(".modal-body").empty();
+        $(".modal-head").empty();
+        var mydesc = $(this).attr("data-desc");
+        var myurl = $(this).attr("data-url");
+
+        var modalDesc = $("<p>")
+        modalDesc.addClass("modalDesc");
+        if (mydesc !== ''){
+        modalDesc.append(mydesc);
+        }
+        else{
+        modalDesc.append("There is no Article to Display, for more information Click on the Link Below.")
+        }
+        $(".modal-body").append(modalDesc);
+
+        var modalLink = $("<a>");
+        modalLink.attr("href",myurl);
+        modalLink.addClass("modalLink");
+        modalLink.text("Click Here to view source");
+        modalLink.attr("target", "blank");
+        $(".modal-body").append(modalLink);
+
+        var mytopic = $(this).attr("data-topic");
+        var modalTopic = $("<p>");
+        modalTopic.addClass("modalTopic");
+        modalTopic.append(mytopic);
+        $(".modal-head").append(modalTopic);
                 
         
         
